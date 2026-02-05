9bc58818e69eadfa65b93812e94eac1c7d8c6a0c
garystarr-surgi <gary.starr@surgishop.com>
2026-02-05 13:50:21 -0500

    Update permission references in hooks.py

--- acquisition/hooks.py
+++ acquisition/hooks.py
@@ -9,17 +9,17 @@ app_license = "MIT"
 
 # 1. Filters the list view
 permission_query_conditions = {
-    "Purchase Order": "your_custom_app.permissions.po_permission_query"
+    "Purchase Order": "acquisition.permissions.po_permission_query"
 }
 
 # 2. Controls direct access/API access
 has_permission = {
-    "Purchase Order": "your_custom_app.permissions.has_po_permission"
+    "Purchase Order": "acquisition.permissions.has_po_permission"
 }
 
 # 3. Runs the automation when creating the record
 doc_events = {
     "Purchase Order": {
-        "before_insert": "your_custom_app.permissions.auto_check_acquisition"
+        "before_insert": "acquisition.permissions.auto_check_acquisition"
     }
 }
