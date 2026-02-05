10c472eae5d757a3a00285fe4202e16fd46df17d
garystarr-surgi <gary.starr@surgishop.com>
2026-02-05 13:37:47 -0500

    Refactor permission handling for Purchase Order

--- acquisition/hooks.py
+++ acquisition/hooks.py
@@ -1,9 +1,7 @@
 # path: your_custom_app/your_custom_app/hooks.py
 
-permission_query_conditions = {
-    "Purchase Order": "your_custom_app.permissions.po_permission_query"
-}
-
-has_permission = {
-    "Purchase Order": "your_custom_app.permissions.has_po_permission"
+doc_events = {
+    "Purchase Order": {
+        "before_insert": "your_custom_app.permissions.auto_check_acquisition"
+    }
 }
