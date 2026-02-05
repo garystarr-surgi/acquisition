e107a669c951c2f63aaaf7ddd4bcce73a2fd531e
garystarr-surgi <gary.starr@surgishop.com>
2026-02-05 13:41:31 -0500

    Add permission filters and automation for Purchase Order

--- acquisition/hooks.py
+++ acquisition/hooks.py
@@ -1,5 +1,16 @@
 # path: your_custom_app/your_custom_app/hooks.py
 
+# 1. Filters the list view
+permission_query_conditions = {
+    "Purchase Order": "your_custom_app.permissions.po_permission_query"
+}
+
+# 2. Controls direct access/API access
+has_permission = {
+    "Purchase Order": "your_custom_app.permissions.has_po_permission"
+}
+
+# 3. Runs the automation when creating the record
 doc_events = {
     "Purchase Order": {
         "before_insert": "your_custom_app.permissions.auto_check_acquisition"
